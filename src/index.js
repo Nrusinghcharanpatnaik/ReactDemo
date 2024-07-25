dfc311dd0b589f87a7c58419c38247eef7fb5659
Nrusinghcharanpatnaik <patnaiksumansonu@gmail.com>
2024-07-25 14:15:23 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
+import Navbar from './Navbar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Navbar />
+      <App />
+    </div>
   </React.StrictMode>
 );
 
