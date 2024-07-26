a6db66717763dafa891db283f8ab4530f1a0e682
Nrusinghcharanpatnaik <patnaiksumansonu@gmail.com>
2024-07-26 14:52:19 +0530

    Initial code setup

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import Navbar from './Navbar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <Navbar />
       <App />
     </div>
   </React.StrictMode>
