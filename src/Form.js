0cb9f1a215eb32e1a30db4b5f1231747f6340acf
Nrusinghcharanpatnaik <patnaiksumansonu@gmail.com>
2024-08-01 15:24:37 +0530

    commit

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,27 @@
-import Input from './Input';
+import Input from "./Input";
+import "./Form.css";
 
 const Form = () => {
   return (
     <div>
-      <div>Sign up Form</div>
-      <Input name="fName" type="text"/>
-      <Input name="lName" type="text"/>
-      <Input name="email" type="email"/>
+      <div class="Sign up">Sign up Form</div>
+      <div class="Form">
+        <Input name="First Name" type="text" />
+        <Input name="Last Name" type="text" />
+        <Input name="email" type="email" />
+      </div>
       <div id="name-error" class="error-container"></div>
+      <div class="button-container">
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
+        <button type="button" class="btn btn-success">
+          Success
+        </button>
+        <button type="button" class="btn btn-info">
+          Info
+        </button>
+      </div>
     </div>
   );
 };
